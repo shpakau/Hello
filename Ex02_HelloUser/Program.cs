38d5e4626ef79b2a45808bd941b341bb27ae4b54
shpakau <shpakau@yahoo.com>
2022-08-31 23:31:11 +0300

    dopolnenie i izmineniz poslednie

--- Ex02_HelloUser/Program.cs
+++ Ex02_HelloUser/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите свое имяgit ");
+﻿Console.Write("Введите свое имя");
 string username = Console.ReadLine ();
 Console.Write("Привет,");
 Console.Write(username);
--- Ex03_Sum/Program.cs
+++ Ex03_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See Sum two numbers
-int numberA = 33;
-int numberB = 5;
+int numberA = new Random().Next(1,10); // 1,2,..9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 1,2,..9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
